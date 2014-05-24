a3154498b02f7b90fb503c32c6cf34d885058e12
Li Jinlong <lijinlongxyz@gmail.com>
2014-05-24 15:10:42 +0800

    translate spine

--- cocos/editor-support/spine/Json.h
+++ cocos/editor-support/spine/Json.h
@@ -21,6 +21,7 @@
  */
 
 /* Esoteric Software: Removed everything except parsing, shorter method names, more get methods, double to float, formatted. */
+/* Esoteric Software: 移除除了解析，简短的名字，get方法, 双精度浮点转换为单精度浮点数（double to float）, 格式化之外的所有方法. */
 
 #ifndef SPINE_JSON_H_
 #define SPINE_JSON_H_
@@ -29,7 +30,7 @@
 extern "C" {
 #endif
 
-/* Json Types: */
+/* Json 类型: */
 #define Json_False 0
 #define Json_True 1
 #define Json_NULL 2
@@ -38,35 +39,35 @@ extern "C" {
 #define Json_Array 5
 #define Json_Object 6
 
-/* The Json structure: */
+/* Json结构: */
 typedef struct Json {
 	struct Json* next;
-	struct Json* prev; /* next/prev allow you to walk array/object chains. Alternatively, use getSize/getItem */
-	struct Json* child; /* An array or object item will have a child pointer pointing to a chain of the items in the array/object. */
+	struct Json* prev; /* next/prev 允许你遍历整个数组/对象. 你还可以使用getSize/getItem达到相同目地 */
+	struct Json* child; /* 数组或者对象可能具有一个child指针， child指向他们的所有子项的链表. */
 
-	int type; /* The type of the item, as above. */
-	int size; /* The number of children. */
+	int type; /* json数据的类型，使用上面定义的Json类型的值. */
+	int size; /* 子节点数量. */
 
-	const char* valueString; /* The item's string, if type==Json_String */
-	int valueInt; /* The item's number, if type==Json_Number */
-	float valueFloat; /* The item's number, if type==Json_Number */
+	const char* valueString; /* 结构的字符串值, 要求 type==Json_String 才有意义 */
+	int valueInt; /* 结构的整型值, 要求 type==Json_Number 才有意义 */
+	float valueFloat; /* 结构的浮点值, 要求 type==Json_Number 才有意义 */
 
-	const char* name; /* The item's name string, if this item is the child of, or is in the list of subitems of an object. */
+	const char* name; /* 结构的名字字符串, 结构式object的子节点子项if this item is the child of, or is in the list of subitems of an object. */
 } Json;
 
-/* Supply a block of JSON, and this returns a Json object you can interrogate. Call Json_dispose when finished. */
+/* 提供JSON数据块, 返回一个Json对象用于后序操作，当操作完成后调用Json_dispose销毁对象. */
 Json* Json_create (const char* value);
 
-/* Delete a Json entity and all subentities. */
+/* 删除Json结构体和他的所有子节点. */
 void Json_dispose (Json* json);
 
-/* Get item "string" from object. Case insensitive. */
+/* 获取Json的Object对象"string"项. 不区分大小写. */
 Json* Json_getItem (Json* json, const char* string);
 const char* Json_getString (Json* json, const char* name, const char* defaultValue);
 float Json_getFloat (Json* json, const char* name, float defaultValue);
 int Json_getInt (Json* json, const char* name, int defaultValue);
 
-/* For analysing failed parses. This returns a pointer to the parse error. You'll probably need to look a few chars back to make sense of it. Defined when Json_create() returns 0. 0 when Json_create() succeeds. */
+/* 如果解析失败，返回解析错误信息的字符串, 你可能要向后查看几个字符来搞清楚它的含义。当Json_create()返回0时被设置. Json_create()成功是返回0 */
 const char* Json_getError (void);
 
 #ifdef __cplusplus
